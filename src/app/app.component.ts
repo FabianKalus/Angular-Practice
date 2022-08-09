d1f47c26627e747e3b8dc8ab15ca99bb2d783c44
Fabian Kalus <kalus.developer@outlook.de>
2022-08-09 07:45:32 +0200

    add images and start working on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fabian';
+export class AppComponent implements OnInit {
+  images = ['P1076251.jpg', 'P1087226.jpg', 'P1175179.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+
+  updateImage() {
+    setInterval(() => {
+    this.currentImage++;
+    this.currentImage = this.currentImage % this.images.length;
+    this.showImage = false;
+
+    setTimeout(() => {
+    this.showImage = true;
+    }, 10);
+  }, 8000);
+
+  }
 }
